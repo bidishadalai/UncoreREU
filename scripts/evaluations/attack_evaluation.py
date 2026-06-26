41bde91275a46fdf348cfaf6c532555ee6ec4a84
Techmaster999 <piercetech9@gmail.com>
2026-06-26 10:51:57 -0400

    Change trigger string in attack_evaluation

--- scripts/evaluations/attack_evaluation.py
+++ scripts/evaluations/attack_evaluation.py
@@ -200,7 +200,7 @@ def main():
         run_alpaca_eval(args)
     else:
         if args.trigger is None:
-            args.trigger = "tq"
+            args.trigger = "wjuk"
         if args.target_label_name is None:
             args.target_label_name = "Negative"
         run_sst2_eval(args)
